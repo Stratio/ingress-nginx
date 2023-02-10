48a9051bf13be8033842f975aeae1c86674da714
Alvaro Campesino <57360851+Alvaro-Campesino@users.noreply.github.com>
2023-02-10 15:04:24 +0100

    [EOS-10808] Set explicit domain for stratio-cookie (#18) (#22)
    Co-authored-by: Miguel Angel Jimenez <majimenez@stratio.com>
    Co-authored-by: Viktor Jacynycz <vjacynycz@stratio.com>
    Co-authored-by: Unai Arríen <unaittxu@gmail.com>
    Co-authored-by: Francisco Orselli <forselli@stratio.com>

--- rootfs/etc/nginx/lua/stratio_auth.lua
+++ rootfs/etc/nginx/lua/stratio_auth.lua
@@ -96,7 +96,7 @@ function _M.create_cookie(userinfo_url, oauth2_cookie_name, stratio_cookie_name,
         end
         return
     end
-    
+
     -- Get request's cookies
     local req_cookie, err = ck:new()
     if not req_cookie then
@@ -118,12 +118,11 @@ function _M.create_cookie(userinfo_url, oauth2_cookie_name, stratio_cookie_name,
     -- If there's no Stratio cookie in the request, add it
     local stratio_cookie, err = req_cookie:get(stratio_cookie_name)
     if not stratio_cookie then
-
         ngx.log(ngx.DEBUG, 'Cookie not found in request')
         stratio_jwt = create_jwt(oauth2_cookie, userinfo_url, signing_key)
-
         ngx.log(ngx.DEBUG, 'Cookie created, adding to response')
-        local ok, err = req_cookie:set({
+
+        local req_cookie_data = {
             key = stratio_cookie_name,
             value = stratio_jwt,
             path = "/",
@@ -132,8 +131,13 @@ function _M.create_cookie(userinfo_url, oauth2_cookie_name, stratio_cookie_name,
             samesite = "Lax",
             expires = ngx.cookie_time(ngx.time() + 21600),
             max_age = 21600
-        })
+        }
+        local cookie_domain = os.getenv("STRATIO_COOKIE_DOMAIN")
+        if cookie_domain then
+            req_cookie_data.domain = cookie_domain
+        end
 
+        local ok, err = req_cookie:set(req_cookie_data)
         if not ok then
             ngx.log(ngx.STDERR, 'Unexpected error setting the Stratio cookie: ', err)
             return 403
@@ -142,7 +146,6 @@ function _M.create_cookie(userinfo_url, oauth2_cookie_name, stratio_cookie_name,
         ngx.log(ngx.DEBUG, 'Adding cookie to request')
         ngx.req.set_header("Cookie", stratio_cookie_name .. "=" .. stratio_jwt .. ";" .. ngx.var.http_cookie);
     else
-
         ngx.log(ngx.DEBUG, 'Cookie found in request, verifying signature, expiration and issuer')
         local jwt = require "resty.jwt"
         local jwt_obj = jwt:verify(verification_key, stratio_cookie, {
@@ -152,12 +155,11 @@ function _M.create_cookie(userinfo_url, oauth2_cookie_name, stratio_cookie_name,
         })
 
         if not jwt_obj["verified"] then
-            
             ngx.log(ngx.DEBUG, 'Invalid JWT, generating a new one')
             stratio_jwt = create_jwt(oauth2_cookie, userinfo_url, signing_key)
-            
             ngx.log(ngx.DEBUG, 'Cookie created, adding to response')
-            local ok, err = req_cookie:set({
+
+            local req_cookie_data = {
                 key = stratio_cookie_name,
                 value = stratio_jwt,
                 path = "/",
@@ -166,8 +168,13 @@ function _M.create_cookie(userinfo_url, oauth2_cookie_name, stratio_cookie_name,
                 samesite = "Lax",
                 expires = ngx.cookie_time(ngx.time() + 21600),
                 max_age = 21600
-            })
-    
+            }
+            local cookie_domain = os.getenv("STRATIO_COOKIE_DOMAIN")
+            if cookie_domain then
+                req_cookie_data.domain = cookie_domain
+            end
+
+            local ok, err = req_cookie:set(req_cookie_data)
             if not ok then
                 ngx.log(ngx.STDERR, 'Unexpected error setting the Stratio cookie: ', err)
                 return 401
@@ -186,7 +193,7 @@ function _M.create_cookie(userinfo_url, oauth2_cookie_name, stratio_cookie_name,
                 end
                 mycookiestr = mycookiestr .. k .. "=" .. v .. ";"
             end
-            
+
             ngx.log(ngx.DEBUG, 'Adding cookie to request')
             ngx.req.set_header("Cookie", stratio_cookie_name .. "=" .. stratio_jwt .. ";" .. mycookiestr);
         else
